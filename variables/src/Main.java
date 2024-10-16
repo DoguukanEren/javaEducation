61774704b1edf637c9e7b204653410cd74437758
Dogukan <93886690+DoguukanEren@users.noreply.github.com>
2024-10-16 18:15:43 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 10; // Tam Sayı Türünde Değişkenler İçin Geçerli Değişkenimzidir.
+        String mesaj = "Metinsel Bir İfade İçermektedir" ;
+
+
+
+    }
+}
